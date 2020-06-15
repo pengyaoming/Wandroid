2aaf5b053fbdb2135b9480f83b3df9f874e87f20
pengym <1329358637@qq.com>
2020-06-15 10:01:28 +0800

    高斯模糊

---
+++ app/src/main/java/com/example/wananzhuo/ui/mine/login/LoginActivity.java
@@ -0,0 +1,11 @@
+package com.example.wananzhuo.ui.mine.login;
+
+/**
+ * Created by Android Studio.
+ * User:
+ * Date: 2020/6/10
+ * Time: 11:17
+ * Name: 作用类：
+ */
+public class LoginActivity {
+}
---
+++ app/src/main/java/com/example/wananzhuo/ui/mine/login/LoginPresenter.java
@@ -0,0 +1,11 @@
+package com.example.wananzhuo.ui.mine.login;
+
+/**
+ * Created by Android Studio.
+ * User: pengym
+ * Date: 2020/6/10
+ * Time: 14:09
+ * Name: 作用类：
+ */
+public class LoginPresenter {
+}
